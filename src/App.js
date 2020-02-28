a633a27798631bad88a1800e764b8ee5c454ea72
Julian Helmholz <39792510+JulHelm@users.noreply.github.com>
2020-02-28 11:21:03 +0100

    added card list component and grid css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-lists/card-list.component';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      monsters: [
-        {
-          id: '1',
-          name: 'Frankenstein'
-        },
-        {
-          id: '2',
-          name: 'Dracula'
-        },
-        {
-          id: '3',
-          name: 'Zombie'
-        }
-      ]
+      monsters: []
     };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
