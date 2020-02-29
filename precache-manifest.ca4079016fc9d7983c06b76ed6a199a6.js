5e8fb8e6342a3b5fb24c8af40782e2d7f96a673a
Julian Helmholz <39792510+JulHelm@users.noreply.github.com>
2020-02-29 11:09:35 +0100

    Updates

---
+++ precache-manifest.ca4079016fc9d7983c06b76ed6a199a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42282d5ea2d83d64fb58802617216359",
+    "url": "/monsters-reloaded/index.html"
+  },
+  {
+    "revision": "1f5ea480e9bad1d91012",
+    "url": "/monsters-reloaded/static/css/main.5c24e698.chunk.css"
+  },
+  {
+    "revision": "edd55aa47d1802b964a5",
+    "url": "/monsters-reloaded/static/js/2.6a194a2a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-reloaded/static/js/2.6a194a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5ea480e9bad1d91012",
+    "url": "/monsters-reloaded/static/js/main.56ccb09a.chunk.js"
+  },
+  {
+    "revision": "548ecf304c5f1f82a6da",
+    "url": "/monsters-reloaded/static/js/runtime-main.c56f6b96.js"
+  }
+]);
